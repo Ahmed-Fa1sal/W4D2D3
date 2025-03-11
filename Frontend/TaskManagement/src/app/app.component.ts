f53de8f487d6fbf5cceae9452d7f03459ae0f35d
Ahmed-Fa1sal <DerRiese765@gmail.com>
2025-03-11 17:22:37 +0300

    connections

--- Frontend/TaskManagement/src/app/app.component.ts
+++ Frontend/TaskManagement/src/app/app.component.ts
@@ -1,15 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { appConfig } from './app.config';  // Import the configuration
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { Task } from './models/task.model';
+import * as TaskActions from './state/task.actions';
+import { selectAllTasks } from './state/task.selectors';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  standalone: true,  // Specify this component as standalone
-  imports: [RouterOutlet],  // Only RouterOutlet in imports
- // Use the configuration in the providers
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'TaskManagement';
+export class AppComponent implements OnInit {
+  tasks$: Observable<Task[]>;
+
+  constructor(private store: Store) {
+    this.tasks$ = this.store.select(selectAllTasks);
+  }
+
+  ngOnInit(): void {
+    this.store.dispatch(TaskActions.loadTasks());
+  }
 }
